81b5025109faf0f697d90d8e36a8ed1dda7445b5
Kubiczeek <jakubakubek@gmail.com>
2024-02-18 23:08:27 +0100

    feat(image): add images for sidebar

--- src/lib/assets/images.js
+++ src/lib/assets/images.js
@@ -1,4 +1,6 @@
 export { default as magnifier } from "./Magnifier.svg";
+export { default as cross } from "./cross.svg";
+export { default as hamburger } from "./hamburger.svg";
 export { default as refresh } from "./Refresh.svg";
 export { default as logo_black } from "./TeacherDigitalAgency_LOGO_black.svg";
 export { default as logo_color } from "./TeacherDigitalAgency_LOGO_colour-black.svg";
