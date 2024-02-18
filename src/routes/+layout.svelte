3bae08e45655bc7254169aafced841ad7efca878
Kubiczeek <jakubakubek@gmail.com>
2024-02-18 23:08:48 +0100

    feat(resposnivity): update responsivty for navbar and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,185 +1,233 @@
 <script>
   import "sanitize.css";
-  import { logo_black } from "$lib/assets/images.js";
+  import {
+    logo_black,
+    hamburger,
+    cross,
+    logo_color,
+  } from "$lib/assets/images.js";
+
+  let open = false;
 </script>
 
 <nav>
-  <a href="/"><img src={logo_black} alt="logo" /> </a>
-  <div class="anchors">
-    <a href="/">Domů</a>
-    <a href="/">Seznam lektorů</a>
-    <a href="/">O nás</a>
-    <a href="/">Kontkat</a>
+  <div class="contents">
+    <a href="/">
+      <img src={logo_black} alt="" />
+    </a>
+    <div class="links">
+      <a href="/">Domů</a>
+      <a href="/">Seznam lektorů</a>
+      <a href="/">O nás</a>
+      <a href="/">Kontakt</a>
+    </div>
+    <button on:click={() => (open = true)}>
+      <img src={hamburger} alt="" />
+    </button>
+    <div class="sidebar" class:open>
+      <button class="close" on:click={() => (open = false)}>
+        <img src={cross} alt="" />
+      </button>
+      <a href="/">Domů</a>
+      <a href="/">Seznam lektorů</a>
+      <a href="/">O nás</a>
+      <a href="/">Kontakt</a>
+    </div>
   </div>
 </nav>
 <slot />
 <footer>
-  <div>
-    <a href="/"><img src={logo_black} alt="logo" /> </a>
-    <div class="anchors">
-      <div class="list">
-        <span class="header">Navigace pro zbloudilé pocestné</span>
-        <a href="/">Lektorský portál</a>
-        <a href="/">Seznam lektorů</a>
-        <a href="/">O nás</a>
-        <a href="/">Kontkat</a>
-      </div>
-      <div class="list">
-        <span class="header">Soukromí</span>
-        <a href="/">GDPR</a>
-        <a href="/">Ochrana osobních údajů</a>
-        <a href="/">Licenční podmínky</a>
-      </div>
-      <div class="list">
-        <span class="header">Ostatní</span>
-        <a href="/">Autoři</a>
-        <a href="/">Technologie</a>
-      </div>
-    </div>
+  <div class="footer-content">
+    <a href="/"> <img src={logo_color} alt="" /> </a>
+    <ul>
+      <li><a href="/">Navigace pro zbloudilé pocestné</a></li>
+      <li><a href="/">Lektorský portál</a></li>
+      <li><a href="/">Seznam lektorů</a></li>
+      <li><a href="/">O nás</a></li>
+      <li><a href="/">Kontakt</a></li>
+    </ul>
+    <ul>
+      <li><a href="/">Soukromí</a></li>
+      <li><a href="/">GDPR</a></li>
+      <li><a href="/">Ochrana osobních údajů</a></li>
+      <li><a href="/">Licenční podmínky</a></li>
+    </ul>
+    <ul>
+      <li><a href="/">Ostatní</a></li>
+      <li><a href="/">Autoři</a></li>
+      <li><a href="/">Technologie</a></li>
+    </ul>
   </div>
 </footer>
-<p class="copywriter">
-  © 2024 Teacher Digital Agency <span>(Ošklivá Káčátka)</span>
-</p>
+<p>© 2024 Teacher Digital Agency <span>(Ošklivá Káčátka)</span></p>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Lalezar&family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&display=swap");
 
   :global(html) {
     font-size: 15px;
+  }
+
+  :global(img) {
+    max-width: 100%;
+  }
+
+  :global(a, span, p, li) {
     font-family: "Open Sans", sans-serif;
-    color: #333;
-    padding: 0;
     margin: 0;
-  }
-
-  a,
-  span,
-  p {
-    color: #333;
+    padding: 0;
     text-decoration: none;
-    padding: 0;
-    margin: 0;
+    color: #333;
   }
 
-  .copywriter {
-    font-size: 0.933rem;
-    font-weight: 600;
-    color: rgba(51, 51, 51, 0.41);
-    text-align: center;
-    /* position: absolute;
-    bottom: 0; */
-    width: 100%;
-  }
-
-  .copywriter span {
-    color: rgba(116, 199, 211, 0.78);
-  }
-
-  .anchors {
-    display: flex;
-    align-items: center;
-    font-size: 1.2rem;
-  }
-
-  nav .anchors {
-    gap: 2.33rem;
-  }
-
-  .list {
-    display: flex;
-    flex-direction: column;
-    gap: 0.6rem;
-    font-size: 0.933rem;
-  }
-
-  .header {
-    font-weight: 700;
-  }
-
-  footer .anchors {
-    align-items: flex-start;
-    gap: 6.66rem;
-  }
-
-  footer .anchors div {
-    align-items: flex-start;
+  :global(ul, ol) {
+    list-style: none;
   }
 
   footer {
     width: 100%;
     display: flex;
-    padding: 30.5px 150px;
-    flex-direction: column;
-    /* position: absolute;
-    bottom: 10px; */
-    -webkit-box-shadow: inset 0px 4px 10.7px 0px rgba(51, 51, 51, 0.01);
-    -moz-box-shadow: inset 0px 4px 10.7px 0px rgba(51, 51, 51, 0.01);
-    box-shadow: inset 0px 4px 10.7px 0px rgba(51, 51, 51, 0.01);
+    justify-content: center;
+    box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.1);
   }
 
-  footer div {
+  footer div > a {
     display: flex;
+    justify-content: center;
     align-items: center;
-    justify-content: space-between;
   }
 
-  img {
-    max-width: 100%;
-    width: 86px;
-  }
-
-  footer img {
+  footer a > img {
     width: 137px;
   }
 
+  footer div {
+    width: min(1200px, 95%);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  footer ul > li {
+    padding: 0.1rem 1.25rem;
+  }
+
+  footer ul > li:first-child {
+    padding: 0.5rem 1.25rem;
+    font-weight: 700;
+  }
+
+  footer ul a {
+    padding: 0;
+  }
+
+  footer ul {
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+  }
+
+  p {
+    font-size: 0.85rem;
+    padding: 0.5rem;
+    font-weight: 600;
+    text-align: center;
+    color: #333;
+    opacity: 0.6;
+  }
+
+  p > span {
+    color: #74c7d3;
+  }
+
   nav {
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    font-weight: 600;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .contents {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     width: 100%;
-    padding: 19px 150px;
-    font-weight: 600;
-    -webkit-box-shadow: 0px 3px 16.2px 0px rgba(51, 51, 51, 0.03);
-    -moz-box-shadow: 0px 3px 16.2px 0px rgba(51, 51, 51, 0.03);
-    box-shadow: 0px 3px 16.2px 0px rgba(51, 51, 51, 0.03);
+    max-width: 1200px;
+    font-size: 1.2rem;
   }
 
-  @media (max-width: 1300px) {
-    nav {
-      padding: 19px 2%;
-    }
-    footer {
-      padding: 30.5px 2%;
-    }
+  .contents button,
+  .sidebar {
+    display: none;
   }
 
-  @media (max-width: 850px) {
-    footer > div > a {
+  a {
+    padding: 1rem 2rem;
+  }
+
+  nav img {
+    width: 86px;
+  }
+
+  @media (max-width: 720px) {
+    .links {
       display: none;
     }
 
-    footer > div {
-      width: 100%;
+    footer div {
+      flex-direction: column;
     }
 
-    footer > div > .anchors {
+    footer div > a {
+      display: none;
+    }
+
+    .sidebar {
       display: flex;
+      position: absolute;
+      flex-direction: column;
+      align-items: flex-end;
+      top: 0;
+      left: -100%;
       width: 100%;
-      justify-content: space-between;
-      gap: 0px;
+      height: 100vh;
+      font-weight: 700;
+      font-size: 1.4rem;
+      padding: 1rem 0.2rem;
+      backdrop-filter: blur(3px);
+      opacity: 0;
+      transition: all 0.3s ease-in-out;
     }
 
-    footer {
-      align-items: center;
+    .sidebar > button {
+      display: block;
+      border: none;
+      background-color: transparent;
+      width: auto;
+      padding: 1rem 2rem;
     }
-  }
 
-  @media (max-width: 500px) {
-    nav > .anchors {
-      gap: 0.3rem;
+    .sidebar > button img {
+      width: 30px;
     }
-    .anchors {
-      font-size: 0.8rem;
+
+    .contents > button {
+      display: block;
+      border: none;
+      background-color: transparent;
+      width: auto;
+      padding: 1rem 2rem;
+    }
+
+    .contents > button img {
+      width: 35px;
+    }
+
+    .open {
+      opacity: 1;
+      left: 0;
     }
   }
 </style>
