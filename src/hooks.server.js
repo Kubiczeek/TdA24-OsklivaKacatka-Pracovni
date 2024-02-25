d30812ed97ab2b4878331ea6efcb912cf0322256
AM1k21 <105818269+AM1k21@users.noreply.github.com>
2024-02-25 22:06:04 +0100

    fix: formatting
    fixxed formatting, i had dissabled my Prettier, now its enabled

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,45 +1,43 @@
 import {
-	reinitializeDB,
-	findTagsUuid,
-	findTagsName,
-	lecturerSchema,
-  } from "$lib/server/db/db.js";
-  import Database from "db-quickly-js";
-  import { redirect } from "@sveltejs/kit"
+  reinitializeDB,
+  findTagsUuid,
+  findTagsName,
+  lecturerSchema,
+} from "$lib/server/db/db.js";
+import Database from "db-quickly-js";
+import { redirect } from "@sveltejs/kit";
 
 export const handle = async ({ event, resolve }) => {
-	// Stage 1
-const {cookies} = event
-const bisc = cookies.get("auth")
-event.locals.user = valCookie(bisc)
+  // Stage 1
+  const { cookies } = event;
+  const bisc = cookies.get("auth");
+  event.locals.user = valCookie(bisc);
 
-	if (event.url.pathname.startsWith("/app")) {
-		if (!event.locals.user) {
-			throw redirect(303, "/login")
-		}
-
-	}
-
-	const response = await resolve(event) // Stage 2
-
-	// Stage 3
-
-	return response
-}
-
-function valCookie(cookie){
-	reinitializeDB()
-	const cluster = Database.getClusterByName("Lecturers");
-	let listOfPossbls = []
-	for (const obj of cluster.data) {
-		listOfPossbls.push(obj.password);
-        
+  if (event.url.pathname.startsWith("/app")) {
+    if (!event.locals.user) {
+      throw redirect(303, "/login");
     }
+  }
 
-	for(const h of listOfPossbls){
-		if(h === cookie){
-			return true
-		}
-	}
-	return false
+  const response = await resolve(event); // Stage 2
+
+  // Stage 3
+
+  return response;
+};
+
+function valCookie(cookie) {
+  reinitializeDB();
+  const cluster = Database.getClusterByName("Lecturers");
+  let listOfPossbls = [];
+  for (const obj of cluster.data) {
+    listOfPossbls.push(obj.password);
+  }
+
+  for (const h of listOfPossbls) {
+    if (h === cookie) {
+      return true;
+    }
+  }
+  return false;
 }
--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -1,86 +1,86 @@
 // Import necessary modules and functions
 import Database from "db-quickly-js";
 import {
-    reinitializeDB,
-    findTagsUuid,
-    findTagsName,
-    lecturerSchema,
+  reinitializeDB,
+  findTagsUuid,
+  findTagsName,
+  lecturerSchema,
 } from "$lib/server/db/db.js";
 import { validatePassword, hashPassword } from "../../../lib/server/hash/hash";
 
 // Function to find password by username in the provided data
 function findPasswordByUsername(username, data) {
-    for (const obj of data) {
-        if (obj.username === username) {
-            return obj.password;
-        }
+  for (const obj of data) {
+    if (obj.username === username) {
+      return obj.password;
     }
-    return false; // Return false if username is not found
+  }
+  return false; // Return false if username is not found
 }
 
 // Handle POST request
 export const POST = async ({ request }) => {
-    // Initialize the database
-    reinitializeDB();
+  // Initialize the database
+  reinitializeDB();
 
-    try {
-        // Parse the JSON payload from the request body
-        const obj = await request.json();
+  try {
+    // Parse the JSON payload from the request body
+    const obj = await request.json();
 
-        // Get the cluster of lecturers from the database
-        const lecturers = Database.getClusterByName("Lecturers");
+    // Get the cluster of lecturers from the database
+    const lecturers = Database.getClusterByName("Lecturers");
 
-        // Find the hashed password for the provided username
-        let hashPass = findPasswordByUsername(obj.username, lecturers.data);
+    // Find the hashed password for the provided username
+    let hashPass = findPasswordByUsername(obj.username, lecturers.data);
 
-        if (!hashPass) {
-            // Return 400 Bad Request if username or password is wrong
-            return new Response(
-                JSON.stringify({ code: 400, message: "Wrong username or password" }),
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    status: 400,
-                }
-            );
-        } else {
-            // Validate the provided password against the hashed password
-            const validation = await validatePassword(hashPass, obj.password);
-
-            if (validation) {
-                // Hash the provided password and return the modified object with status 200
-                obj.password = hashPass;
-                return new Response(JSON.stringify(obj), {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    status: 200,
-                });
-            } else {
-                // Return 400 Bad Request if username or password is wrong
-                return new Response(
-                    JSON.stringify({ code: 400, message: "Wrong username or password" }),
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        status: 400,
-                    }
-                );
-            }
+    if (!hashPass) {
+      // Return 400 Bad Request if username or password is wrong
+      return new Response(
+        JSON.stringify({ code: 400, message: "Wrong username or password" }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 400,
         }
-    } catch (error) {
-        // Handle any unexpected errors
-        console.error("An error occurred:", error);
+      );
+    } else {
+      // Validate the provided password against the hashed password
+      const validation = await validatePassword(hashPass, obj.password);
+
+      if (validation) {
+        // Hash the provided password and return the modified object with status 200
+        obj.password = hashPass;
+        return new Response(JSON.stringify(obj), {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 200,
+        });
+      } else {
+        // Return 400 Bad Request if username or password is wrong
         return new Response(
-            JSON.stringify({ code: 500, message: "Internal Server Error" }),
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                status: 500,
-            }
+          JSON.stringify({ code: 400, message: "Wrong username or password" }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            status: 400,
+          }
         );
+      }
     }
+  } catch (error) {
+    // Handle any unexpected errors
+    console.error("An error occurred:", error);
+    return new Response(
+      JSON.stringify({ code: 500, message: "Internal Server Error" }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        status: 500,
+      }
+    );
+  }
 };
