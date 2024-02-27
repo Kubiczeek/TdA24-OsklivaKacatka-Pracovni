abb3280a97e0635d1fa6a764b1ebbcb9ab9ac11e
Kubiczeek <jakubakubek@gmail.com>
2024-02-27 18:39:59 +0100

    fix: restructured so I have more freedom with layouts

---
+++ src/routes/app/login/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "sanitize.css";
+  import { logo_black } from "$lib/assets/images.js";
+</script>
+
+<svelte:head>
+  <title>TdA - Lektorský Portál - Login</title>
+</svelte:head>
+<img src={logo_black} alt="" />
+<slot />
+
+<style></style>
