3990b50e3f9ee6ab0f878ad057131e0cfe59f932
Kubiczeek <jakubakubek@gmail.com>
2024-02-27 19:13:59 +0100

    feat: ui for login page

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,36 @@
+<slot />
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Lalezar&family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&display=swap");
+
+  :global(html) {
+    font-size: 15px;
+  }
+
+  :global(img) {
+    max-width: 100%;
+  }
+
+  :global(a, span, p, li, a:visited) {
+    font-family: "Open Sans", sans-serif;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color: #333;
+  }
+
+  :global(ul, ol) {
+    list-style: none;
+  }
+
+  :global(button, input) {
+    font-family: "Open Sans", sans-serif;
+    outline: none;
+    border: none;
+    background-color: transparent;
+  }
+
+  :global(button) {
+    cursor: pointer;
+  }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,150 @@
+<script>
+  import "sanitize.css";
+  import { logo_black } from "$lib/assets/images.js";
+</script>
+
+<svelte:head>
+  <title>TdA - Lektorský Portál - Login</title>
+</svelte:head>
+
+<a href="/">
+  <img src={logo_black} alt="" />
+</a>
+<div class="wrapper">
+  <div class="login">
+    <p class="ff-Lalezar">TDA - Lektorský Portál</p>
+    <div class="form-login">
+      <div class="credentials">
+        <div>
+          <input required type="text" name="username" id="" />
+          <label for="username" class="blue">username</label>
+        </div>
+        <div>
+          <input required type="password" name="username" id="" />
+          <label for="username" class="blue">password</label>
+        </div>
+      </div>
+      <div class="footer">
+        <button>Přihlásit se</button>
+        <p>
+          Zapomněli jste heslo? Klikněte <span class="blue">zde</span> pro řešení
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  img {
+    width: 86px;
+    position: absolute;
+    top: 1.3rem;
+    left: 1.3rem;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+    min-height: 90vh;
+    width: min(95%, 500px);
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  .login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+    padding: 1.5rem 3rem;
+    width: auto;
+    border-radius: 15px;
+    -webkit-box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+    -moz-box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+    box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+  }
+
+  .form-login {
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+  }
+
+  .credentials {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
+
+  .footer {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  button {
+    background-color: #74c7d3;
+    color: white;
+    padding: 0.7rem 1.5rem;
+    border-radius: 5px;
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+
+  .credentials div {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  input {
+    border-bottom: 1px solid #74c7d3;
+    padding: 0.2rem;
+    min-width: 400px;
+    font-size: 1.2rem;
+  }
+
+  label {
+    font-family: "Open Sans", sans-serif;
+    position: absolute;
+    left: 0;
+    font-size: 1.2rem;
+    transition: all 0.3s;
+    padding-left: 2px;
+  }
+
+  input:focus ~ label,
+  input:valid ~ label {
+    transform: translateY(-1rem);
+    font-size: 0.8rem;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  .blue {
+    color: #74c7d3;
+  }
+
+  .ff-Lalezar {
+    font-family: "Lalezar", sans-serif;
+    font-size: 2.5rem;
+  }
+
+  @media screen and (max-width: 500px) {
+    .credentials div {
+      width: 100%;
+    }
+    input {
+      min-width: 100%;
+    }
+
+    .ff-Lalezar {
+      font-size: 2rem;
+    }
+  }
+</style>
