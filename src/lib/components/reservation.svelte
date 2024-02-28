66d5e8c05c02ef34e1ced846145e6c005f83f01b
Kubiczeek <jakubakubek@gmail.com>
2024-02-28 23:22:23 +0100

    feat: start of reservation

---
+++ src/lib/components/reservation.svelte
@@ -0,0 +1,157 @@
+<script>
+  let more = true;
+</script>
+
+<div class="container" class:more>
+  <div class="always">
+    <p class="ff-Lalezar">Adam Mikulič</p>
+    <div class="time-date">
+      <span>10.02.2024</span>
+      <div />
+      <span>7:30 - 8:30</span>
+    </div>
+    <p class="sub">
+      Místo: <span class="red-text">Určit místo (klikni)</span>
+    </p>
+    <p class="sub">Téma: Doučování matematiky</p>
+    <p class="sub">Věk: 16</p>
+  </div>
+  <div class="optional">
+    <p class="ff-Lalezar">kontaktní údaje</p>
+    <a class="sub" href="mailto:">Email: schenk@gmail.com</a>
+    <p class="sub" href="">Telefonní číslo: +420 123 456 879</p>
+    <div class="note">
+      <p>Poznámka:</p>
+      <p class="note-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum est minima
+        omnis porro, aliquid dicta facere quibusdam, impedit quidem hic delectus
+        animi qui maxime accusamus! Rem at deserunt impedit fuga mollitia amet,
+        officia repellat quasi vitae! Commodi doloremque eligendi sed ullam illo
+        nam. Similique, doloremque!
+      </p>
+    </div>
+    <div class="buttons">
+      <button class="accept">Potvrdit schůzku</button>
+      <button class="cancel">Zrušit schůzku</button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .ff-Lalezar {
+    font-family: "Lalezar", sans-serif;
+    font-size: 1.25rem;
+  }
+
+  .red-text {
+    color: #ff4040;
+  }
+
+  .container {
+    position: relative;
+    display: flex;
+    padding: 0.33rem 1.33rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    max-height: 150px;
+    overflow: hidden;
+    -webkit-box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+    -moz-box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+    box-shadow: 0px 2px 15px 0px rgba(51, 51, 51, 0.06);
+    border-radius: 8px;
+    transition: all 0.3s;
+  }
+
+  .container:hover {
+    max-height: 550px;
+  }
+
+  .always {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.33rem;
+  }
+
+  .time-date {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
+
+  .time-date div {
+    width: 2px;
+    background-color: rgba(51, 51, 51, 0.17);
+    border-radius: 50px;
+  }
+
+  .time-date span {
+    font-size: 1rem;
+    padding: 0 1rem;
+  }
+
+  .always .sub {
+    font-size: 0.9rem;
+    width: 100%;
+  }
+
+  .optional {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    gap: 0.33rem;
+  }
+
+  .optional .ff-Lalezar {
+    font-size: 1rem;
+    width: 100%;
+  }
+
+  .optional .sub {
+    width: 100%;
+    font-size: 0.9rem;
+  }
+
+  .note {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 250px;
+    border: 1px solid #b8b8b8;
+    border-radius: 4px;
+    padding: 0.3rem;
+    gap: 0.33rem;
+  }
+
+  .note-text {
+    width: 100%;
+    text-wrap: wrap;
+    font-size: 0.9rem;
+  }
+
+  .buttons {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    margin-top: 0.3rem;
+  }
+
+  .buttons button {
+    padding: 0.1rem 0.5rem;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .accept {
+    background-color: #74c7d3;
+    color: white;
+  }
+
+  .cancel {
+    background-color: #e46464;
+    color: white;
+  }
+</style>
---
+++ src/routes/app/reservation/+page.svelte
@@ -0,0 +1,254 @@
+<script>
+  import { logo_black, hamburger, cross } from "$lib/assets/images.js";
+  import toast, { Toaster } from "svelte-french-toast";
+  import Reservation from "$lib/components/reservation.svelte";
+
+  let open = false;
+</script>
+
+<svelte:head>
+  <title>TdA - Lektorský Portál - Info</title>
+</svelte:head>
+
+<Toaster />
+<nav>
+  <img src={logo_black} alt="" />
+  <button on:click={() => (open = true)}>
+    <img src={hamburger} alt="" />
+  </button>
+</nav>
+<div class="sidebar-m" class:open>
+  <button class="close" on:click={() => (open = false)}>
+    <img src={cross} alt="" />
+  </button>
+  <a href="/app/reservation">Rezervace</a>
+  <a href="/app/settings">Nastavení rezervací</a>
+  <a href="/app/info">Osobní informace</a>
+  <a href="/logout">Odhlásit se</a>
+</div>
+<div class="wrapper">
+  <div class="sidebar">
+    <div class="top-side">
+      <a href="/">
+        <img src={logo_black} alt="" />
+      </a>
+      <div class="links">
+        <a class="ff-Lalezar" href="/app/reservation">Rezervace</a>
+        <a class="ff-Lalezar active" href="/app/settings"
+          >Nastavení&nbsp;rezervací</a
+        >
+        <a class="ff-Lalezar" href="/app/info">Osobní&nbsp;informace</a>
+      </div>
+    </div>
+    <div class="bot-side">
+      <a href="/logout" class="ff-Lalezar">Odhlásit&nbsp;se</a>
+    </div>
+  </div>
+  <div class="content">
+    <h1 class="ff-Lalezar">Naplánované schůzky</h1>
+    <div class="allow-reservations">
+      <label for="allowReservations">Aktivní systém rezervací:</label>
+      <input type="checkbox" id="allowReservations" />
+    </div>
+    <div class="sort">
+      <label for="select">Seřadit podle:</label>
+      <select name="select" id="" class="sort">
+        <option value="1">Abecedně A-Z</option>
+        <option value="2">Abecedně Z-A</option>
+        <option value="3">Nejnovější</option>
+        <option value="4">Nejstarší</option>
+      </select>
+    </div>
+    <div class="reservations">
+      <Reservation />
+      <Reservation />
+    </div>
+  </div>
+</div>
+
+<style>
+  h1 {
+    padding: 0;
+    margin: 0;
+  }
+
+  .content h1 {
+    font-size: 3rem;
+    width: 100%;
+    text-align: left;
+    color: #333333;
+  }
+
+  .ff-Lalezar {
+    font-family: "Lalezar", sans-serif;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .sidebar {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    -webkit-box-shadow: 2px 0px 15px 0px rgba(51, 51, 51, 0.06);
+    -moz-box-shadow: 2px 0px 15px 0px rgba(51, 51, 51, 0.06);
+    box-shadow: 2px 0px 15px 0px rgba(51, 51, 51, 0.06);
+    padding: 1.33rem 2rem;
+    height: auto;
+    min-height: 100vh;
+  }
+
+  .sidebar a {
+    font-size: 1.733rem;
+  }
+
+  .top-side {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .links a {
+    text-decoration: underline solid #3333332a;
+  }
+
+  .links a.active {
+    text-decoration: underline solid #333333;
+  }
+
+  .sidebar img {
+    width: 86px;
+  }
+
+  .content {
+    width: 100%;
+    display: flex;
+    padding: 2rem 6.5rem;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  nav,
+  .sidebar-m {
+    display: none;
+  }
+
+  select {
+    border: none;
+    font-size: 1rem;
+    width: fit-content;
+  }
+
+  select:focus {
+    outline: none;
+  }
+
+  option {
+    font-family: "Open Sans", sans-serif;
+  }
+
+  input,
+  label {
+    font-size: 1.15rem;
+    font-family: "Open Sans", sans-serif;
+    color: #333;
+    font-weight: 700;
+  }
+
+  .allow-reservations {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .sort select,
+  .sort label {
+    font-weight: 400;
+    font-size: 1rem;
+    color: #b8b8b8;
+  }
+
+  .reservations {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  @media (max-width: 1000px) {
+    .sidebar {
+      height: auto;
+    }
+
+    .content {
+      padding: 2rem 1.5rem;
+    }
+  }
+
+  @media (max-width: 656px) {
+    nav {
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    nav > img {
+      width: 70px;
+    }
+
+    nav > button > img {
+      width: 34px;
+    }
+
+    .sidebar {
+      display: none;
+    }
+
+    .sidebar-m {
+      display: flex;
+      position: fixed;
+      flex-direction: column;
+      align-items: flex-end;
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 100vh;
+      font-weight: 700;
+      font-size: 1.4rem;
+      padding: 1rem 2rem;
+
+      backdrop-filter: blur(3px);
+      opacity: 0;
+      transition: all 0.3s ease-in-out;
+    }
+
+    .sidebar-m > button {
+      display: block;
+      border: none;
+      background-color: transparent;
+      width: auto;
+      padding: 1rem 0;
+    }
+
+    .sidebar-m > button img {
+      width: 30px;
+    }
+
+    .open {
+      opacity: 1;
+      left: 0;
+    }
+
+    label {
+      font-size: 0.9rem;
+    }
+  }
+</style>
